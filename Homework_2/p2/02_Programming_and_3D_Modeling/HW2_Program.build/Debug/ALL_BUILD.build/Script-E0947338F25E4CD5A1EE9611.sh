d8e81899cadb2ce16a4aae84328c7d433a61fe60
Fei Xu <fei@feixu.student.iastate.edu>
2015-09-17 15:18:52 -0500

    added homework2

---
+++ Homework_2/p2/02_Programming_and_3D_Modeling/HW2_Program.build/Debug/ALL_BUILD.build/Script-E0947338F25E4CD5A1EE9611.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/fei/Documents/homework/com_graph/HCI-557-CG/02_Programming_and_3D_Modeling -f /Users/fei/Documents/homework/com_graph/HCI-557-CG/02_Programming_and_3D_Modeling/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
---
+++ Homework_2/p2/02_Programming_and_3D_Modeling/HW2_Program.build/Debug/ZERO_CHECK.build/Script-375A3BCC3E174CC28DA1ACF2.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/fei/Documents/homework/com_graph/HCI-557-CG/02_Programming_and_3D_Modeling -f /Users/fei/Documents/homework/com_graph/HCI-557-CG/02_Programming_and_3D_Modeling/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
