b6b96e418288b018505622d641d3f72e3a1d4e37
Fei Xu <fei@feixu.student.iastate.edu>
2015-09-17 15:20:04 -0500

    remove

--- homework/p2/02_Programming_and_3D_Modeling/HW2_Program.build/Debug/ZERO_CHECK.build/Script-375A3BCC3E174CC28DA1ACF2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/fei/Documents/homework/com_graph/HCI-557-CG/02_Programming_and_3D_Modeling -f /Users/fei/Documents/homework/com_graph/HCI-557-CG/02_Programming_and_3D_Modeling/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
